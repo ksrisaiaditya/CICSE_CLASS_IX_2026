658c9b1cd330fd31987c8aab5a1a75f1e1034eda
ksrisaiaditya <ksrisaiaditya@gmail.com>
2026-06-20 13:33:41 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
